078de114b6546e227bd60c7850422103ff133213
Gabriel <dubois.gabriel@hotmail.com>
2020-11-27 09:02:49 +0100

    Interface pour les algos

--- Code/IterativeDeepening.py
+++ Code/IterativeDeepening.py
@@ -1,6 +1,7 @@
 import Goban
 import time
 from random import choice
+from AlgoIAInterface import *
 
 # CurrentDepth impair => Niveau Ami (1, 3, ..)
 # CurrentDepth pair => Niveau Ennemi (0, 2, 4, ..)
@@ -16,14 +17,14 @@ class Movement:
     def __repr__(self):
         return f"move=[{self.move}], index=[{self.index}], depth=[{self.depth}], score=[{self.score}]"
 
-class IterativeDeepening:
+class IterativeDeepening(AlgoIAInterface):
 
     #############################################
     #############################################
     ''' Main '''
 
-    def __init__(self, board, duration = 15):
-        self._board = board
+    def __init__(self, board, duration = 5):
+        AlgoIAInterface.__init__(self, board)
         self._itDeepDuration = duration
         self._stopDepth = 1
         self._timeToStop = 0
@@ -65,14 +66,12 @@ class IterativeDeepening:
         Heuristique d'un plateau de jeu de GO avec 9x9 cases 
         TODO : Avoir un objet pour gérer les heuristiques
         '''
-        if isFriendLevel:
-            l = [i for i in range(1, 100)]
-            return choice(l)
-        else:
-            l = [i for i in range(-100, -1)]
-            return choice(l)
+        l = choice([i for i in range(1, 100)])
+        if not isFriendLevel:
+            l *= -1
+        return l
 
-
+            ####
 
     def _startAlphaBeta(self):
         isFriendLevel = True
@@ -109,6 +108,7 @@ class IterativeDeepening:
         move = choice(possibleMoves)
         return move
 
+            ####
 
     def _alphaBeta(self, currentDepth, alpha, beta, isFriendLevel):
         if time.time() >= self._timeToStop:
@@ -117,18 +117,9 @@ class IterativeDeepening:
         if currentDepth == self._stopDepth or self._board.is_game_over():
             return self._boardValue(isFriendLevel)
 
-        ''' 
-        Plutot que de tester 2 fois si un mouvement est legal (ce qui coûte très chère), 
-        on prend le risque d'utiliser des mouvements weak.
-        De toute manière, le mouvement est testé pendant le push, donc pas besoin de tester avant... 
-        Si il est weak, on pop et on passe au suivant, c'est beaucoup plus rentable.
-        '''
         moves = self._board.weak_legal_moves()
         
         firstMove = 0
-
-        '''if currentDepth in self._bestMoves:
-            firstMove = self._bestMoves[currentDepth].index'''
 
         for i in range(firstMove, len(moves)):
 
@@ -148,34 +139,8 @@ class IterativeDeepening:
                 alpha = max(alpha, retValue)
             else:
                 beta = min(beta, retValue)
-                
-                '''
-                if currentDepth in self._bestMoves:
-                    if self._bestMoves[currentDepth].score < retValue:
-                        self._bestMoves[currentDepth] = Movement(moves[i], i, currentDepth, retValue)
-                else:
-                    self._bestMoves[currentDepth] = Movement(moves[i], i, currentDepth, retValue)
-                '''
 
             if alpha >= beta :
                 return (beta if isFriendLevel else alpha)
 
         return (alpha if isFriendLevel else beta)
-
-
-
-    #############################################
-    #############################################
-    ''' Delegation Goban functions '''
-    
-    def _push(self, move):
-        return self._board.push(move)
-
-    def _pop(self):
-        self._board.pop()
-    
-
-
-
-
-
---
+++ Code/monteCarlo.py
@@ -0,0 +1,12 @@
+import Goban
+import time
+from AlgoIAInterface import *
+from random import choice
+
+class MonteCarlo(AlgoIAInterface):
+
+    def __init__(self, board):
+        AlgoIAInterface.__init__(self, board)
+
+    
+
